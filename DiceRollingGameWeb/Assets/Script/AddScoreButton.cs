a27ba1a04f3436daf182803ed338c77104ce4741
zmweizhang-ise <mingw.zhang123@gmail.com>
2022-07-29 16:24:38 -0400

    adding in ui and scripts

---
+++ DiceRollingGameWeb/Assets/Script/AddScoreButton.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AddScoreButton : MonoBehaviour, InterfaceDataPersistance
+{
+    [SerializeField] private TextMeshProUGUI scoreText;
+
+    private int score = 0;
+
+    private void Update()
+    {
+        DisplayScore();
+    }
+    public void AddPointClick()
+    {
+        score = score + 1;
+    }
+
+    public void MinusPointClick()
+    {
+        score = score - 1;
+    }
+
+    private void DisplayScore()
+    {
+        scoreText.text = score + "";
+    }
+
+
+    public void LoadData(PlayerData data)
+    {
+        this.score = data.scores;
+    }
+
+    public void SaveData(ref PlayerData data)
+    {
+        data.scores = this.score;
+
+    }
+}
